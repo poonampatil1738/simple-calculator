a141ffcc9851debff3ba125d7c863626522f853a
Poonam patil <piyu1738@gmail.com>
2025-08-02 20:05:28 +0530

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,20 +1,25 @@
 num1=float(input("enter first number="))
 num2=float(input("enter second number="))
+
 print("enter your choice")
 print("1.Add")
 print("2.Substract")
 print("3.Multiply")
 print("4.Divide")
 choice=int(input("enter your choice="))
+
 if(choice==1):
    add=num1+num2
    print("The result of addition is=",add)
+
 elif(choice==2):
    sub=num1-num2
    print("The result of substration is=",sub)
+
 elif(choice==3):
    mul=num1*num2
    print("The result of multiplication is=",mul)
+
 elif(choice==4):
    if num2!=0:
        div=num1/num2
@@ -27,3 +32,4 @@ else:
 
 
    
+
